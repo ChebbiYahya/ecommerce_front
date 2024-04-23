46e65d1f47d3020e45f5d9ee22d4bcde2e049282
Yahya Chebbi <yahya.chebbi@etudiant-fst.utm.tn>
2024-04-23 18:34:17 +0100

    home page , header , footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'header' , component:HeaderComponent},
+  {path:'',component:HomeComponent},
+  {path: 'footer' , component:FooterComponent},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
